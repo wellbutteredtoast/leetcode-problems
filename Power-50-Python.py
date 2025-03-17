ec2a0e06346047a2df06f919833777311b6d0fa2
c0smic <swatsonjones@mun.ca>
2025-03-17 09:53:51 -0230

    Problem Batch #2 (feat. C++)

--- Power-50-Python.py
+++ Power-50-Python.py
@@ -1,6 +1,6 @@
 # LeetCode Problem #50 -- Power
 # Language: Python
-# Runtime: ~0ms (??)
+# Runtime: < 0ms
 # Memory: 17.71mb
 # Date: 17 March, 2025
 
@@ -11,6 +11,3 @@ def power(x: float, n: int) -> float:
     if x == 0: return 0.0
 
     return float(x**n)
-
-# Note: I have no idea how the runtime is under 1ms.
-#       I won't complain, it's the fastest on LeetCode.
---
+++ SquareRoot-69-CPP.cpp
@@ -0,0 +1,16 @@
+// LeetCode Problem #69 -- sqrt
+// Language: C++
+// Runtime: < 0ms
+// Memory: 8.38mb
+// Date: 17 March, 2025
+
+int sqrt(int x) {
+    if (x == 0) return 0;
+    long r = x;
+    while (r * r > x) {
+        r = (r + x / r) / 2;
+    }
+    return r;
+}
+
+// Note: Good method, insanely fast, and using very little memory across a couple thousand cases.¬
